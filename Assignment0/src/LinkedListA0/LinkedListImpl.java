79dff9986025b5cc8436e4df2ca4f10b30f05e71
Chathan Driehuys <cdriehuys@gmail.com>
2016-09-04 17:19:35 -0400

    Add clear and size methods to list.

--- Assignment0/src/LinkedListA0/LinkedListImpl.java
+++ Assignment0/src/LinkedListA0/LinkedListImpl.java
@@ -15,7 +15,33 @@ public class LinkedListImpl implements LIST_Interface {
         root = new Node(0);
     }
 
+    /**
+     * Clear the list.
+     *
+     * To clear the list, the {@code next} attribute of the root
+     * node is set to {@code null}.
+     */
+    public void clear() {
+        root.next = null;
+    }
+
     public Node getRoot() {
         return root;
+    }
+
+    /**
+     * Get the number of items in the list.
+     * @return The number of {@code Node} instances in the list.
+     */
+    public int size() {
+        Node current = root;
+        int size = 0;
+
+        while (current.getNext() != null) {
+            size++;
+            current = current.getNext();
+        }
+
+        return size;
     }
 }
